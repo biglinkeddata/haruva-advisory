11d56b59fb44657cb1dea40baf02a4e025c4e390
Gareth Breach <gareth.breach@gmail.com>
2025-11-07 13:02:40 +0000

    git pages stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'  // or vue, svelte, etc.
+import react from '@vitejs/plugin-react-swc'  // or vue, svelte, etc.
 
 export default defineConfig({
   plugins: [react()],
