d55a0713dcdd5d3eae2f6c71110b6a36799e36c0
Gareth Breach <gareth.breach@gmail.com>
2025-11-07 13:04:54 +0000

    git pages stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'  // or vue, svelte, etc.
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   base: '/haruva-advisory/', // 👈 your repo name here
 })
